f7b60f3376196cd201a99390b40999ebaa817f12
KSkendere <kristineskendere@inbox.lv>
2021-10-06 22:09:52 +0300

    adding lesson2

---
+++ src/lesson2/syntax/Fundamentals.java
@@ -0,0 +1,35 @@
+package lesson2.syntax;
+
+/**
+ *
+ */
+public class Fundamentals {
+    public static void main(String[] args) {
+
+// COMMENTS
+
+//        We will write simple one line comments here
+//        And another line that starts with double line
+        /*
+        line 1 of the Comment
+        line 2 of the comment
+         */
+    }
+
+    /**
+     * someMethod should be used in following cases - when the method should be used
+     *
+     * @param param1 - name of user
+     * @param parm2
+     * @return
+     */
+
+    public String someMethod(String param1, int parm2) {
+        return "something";
+    }
+/*-
+rgjjhguhuwuhuuhuhuhuhu
+ */
+}
+
+
