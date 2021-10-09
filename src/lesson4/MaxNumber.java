101a37fcdec695f9d98b87eb2abe6e3d3cf8bff5
KSkendere <kristineskendere@inbox.lv>
2021-10-09 15:06:26 +0300

    adding lesson4 changed

--- src/lesson4/MaxNumber.java
+++ src/lesson4/MaxNumber.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Array;
 public class MaxNumber {
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 4, 5,2,5,6,2,4};
+        int[] nums = {2,3,4,5};
 
         int max = Integer.MIN_VALUE;
         int max2 = Integer.MIN_VALUE;
@@ -13,29 +13,14 @@ public class MaxNumber {
 
         for (int i = 0; i < nums.length; i++) {
 
-            if (nums.length <= 1 || nums.length >= Math.pow(10, 4)) {
-                System.out.println("Array is not correct");
-                break;
-            }
-            if (nums[i] <= Math.pow(-2, 31) || nums[i] >= Math.pow(2, 31) - 1) {
-                System.out.println("Number is not correct");
-                break;
-            }
+
             if (max < nums[i]) {
                 max = nums[i];
             }
         }
         for (int j = 0; j < nums.length; j++) {
 
-            if (nums.length <= 1 || nums.length >= Math.pow(10, 4)) {
-                System.out.println("Array is not correct");
-                break;
-            }
 
-            if (nums[j] <= Math.pow(-2, 31) || nums[j] >= Math.pow(2, 31) - 1) {
-                System.out.println("Number is not correct");
-                break;
-            }
 
             if (max2 < nums[j] && nums[j] != max) {
                 max2 = nums[j];
@@ -44,14 +29,7 @@ public class MaxNumber {
 
         for (int n = 0; n < nums.length; n++) {
 
-            if (nums.length <= 1 || nums.length >= Math.pow(10, 4)) {
-                System.out.println("Array is not correct");
-                break;
-            }
-            if (nums[n] <= Math.pow(-2, 31) || nums[n] >= Math.pow(2, 31) - 1) {
-                System.out.println("Number is not correct");
-                break;
-            }
+
             if (max3 < nums[n] && nums[n] != max && nums[n] != max2) {
                 max3 = nums[n];
             }
