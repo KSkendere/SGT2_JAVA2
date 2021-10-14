030ffe8885866e7851a75768ad1b544ebac9136f
KSkendere <kristineskendere@inbox.lv>
2021-10-14 23:28:17 +0300

    adding some changes in lesson 4

--- src/lesson4_KSkendere/RomanNumerals4.java
+++ src/lesson4_KSkendere/RomanNumerals4.java
@@ -3,7 +3,6 @@ package lesson4_KSkendere;
 public class RomanNumerals4 {
     public static void main(String[] args) {
 
-        int result;
 
         System.out.println(romanNumeral(25));
         System.out.println(romanNumeral(50));
@@ -19,8 +18,8 @@ public class RomanNumerals4 {
     /**
      * converts integer to roman numeral in range from 1 till 3999
      *
-     * @param integerToConvert
-     * @return
+     * @param integerToConvert : integer to be converted in to roman numeral
+     * @return convertedInteger: String made from String arrays
      */
     public static String romanNumeral(int integerToConvert) {
 
@@ -30,10 +29,12 @@ public class RomanNumerals4 {
         String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
-        return  thousands[integerToConvert/1000]+
+        String convertedInteger = "";
+                convertedInteger= convertedInteger+
+                thousands[integerToConvert/1000]+
                 hundreds[(integerToConvert%1000)/100]+
                 tens [(integerToConvert%100)/10]+
                 ones[integerToConvert%10];
-
+        return convertedInteger;
     }
 }
